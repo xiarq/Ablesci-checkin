23f4f79e17b9c4c8be9cba04a9f59e237d6945fb
xiarq <64955517+xiarq@users.noreply.github.com>
2024-08-30 08:46:36 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding=utf-8
+'''
+Author: JY
+Date: 2023-12-27 18:30:32
+LastEditTime: 2023-12-27 21:02:08
+LastEditors: JY
+Description: 
+FilePath: /Ablesci-checkin/main.py
+'''
+#!/usr/bin/env python
+# coding=utf-8
+import requests
+import os
+import json
+env = os.environ.get("CONFIG")
+
+
+def ablesci(headers):
+    url = "https://www.ablesci.com/user/sign"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+
+
+if __name__ == "__main__":
+    config_path = os.path.join(os.path.dirname(__file__), "config.json")
+    if os.path.exists(config_path):
+        with open(config_path, "r", encoding="utf-8") as f:
+            config = json.loads(f.read())
+    else:
+        config = json.loads(env)
+    
+
+    _check_item = config.get("ablesci", [])[0]
+    pushToken = config.get("PUSHTOKEN", [])
+    
+
+    Cookie = _check_item.get("cookie",[])
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Cookie": f"{Cookie}",
+        "DNT": "1",
+        "Referer": "https://www.ablesci.com/",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+
+    if pushToken != None:
+        msg = ablesci(headers=headers)
+        content = "今日科研通签到！\n" + str(msg)
+        requests.post('https://www.pushplus.plus/send', { 'token': pushToken, 'title': '科研通签到', 'content': content });
